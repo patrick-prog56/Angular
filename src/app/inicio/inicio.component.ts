f92c9898c57d074d8ff282b8171425e607569cb2
Patrick <patricksantana56@gmail.com>
2021-07-09 01:39:11 -0300

    implementação do Get e Post de postagens

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
 
@@ -15,6 +16,7 @@ import { TemaService } from '../service/tema.service';
 export class InicioComponent implements OnInit {
 
   postagem: Postagem= new Postagem()
+  listaPostagens: Postagem[]
 
   tema:Tema= new Tema()
   listaTemas: Tema[]
@@ -27,7 +29,8 @@ export class InicioComponent implements OnInit {
   constructor(
     private router : Router,
     private postagemService: PostagemService,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
@@ -36,6 +39,7 @@ export class InicioComponent implements OnInit {
       this.router.navigate(['/entrar'])
     }
     this.getAllTemas()
+    this.getAllPostagens()
 
     console.log(environment.id)
   }
@@ -48,6 +52,19 @@ export class InicioComponent implements OnInit {
   findByIdTema(){
     this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
       this.tema=resp
+    })
+  }
+
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp:Postagem[])=>{
+      this.listaPostagens=resp
+    })
+  }
+
+  findByIdUser(){
+    this.authService.getByIdUser(this.idUser).subscribe((resp: User)=>{
+      this.user = resp
     })
   }
 
@@ -66,6 +83,7 @@ export class InicioComponent implements OnInit {
       this.postagem = resp
       alert('Parabéns, sua postagem foi realizada com sucesso!!!')
       this.postagem = new Postagem()
+      this.getAllPostagens()
     })
   }
 
