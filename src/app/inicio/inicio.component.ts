abf83f289671e670980c307b8039c7a2f82f6be8
Patrick <patricksantana56@gmail.com>
2021-07-13 01:21:25 -0300

    Adicionando função de pesquisa ao blog pessoal

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -18,10 +18,12 @@ export class InicioComponent implements OnInit {
 
   postagem: Postagem= new Postagem()
   listaPostagens: Postagem[]
+  tituloPost: string
 
   tema:Tema= new Tema()
   listaTemas: Tema[]
   idTema:number
+  nomeTema: string
 
   user: User = new User()
   idUser= environment.id
@@ -43,7 +45,7 @@ export class InicioComponent implements OnInit {
     window.scroll(0,0)
 
     if (environment.token==''){
-      alert('Ops! sua sessão expirou, faça o login novamente !!!')
+      this.alertas.showAlertDanger('Ops! sua sessão expirou, faça o login novamente !!!')
       this.router.navigate(['/entrar'])
     }
     this.getAllTemas()
@@ -95,4 +97,27 @@ export class InicioComponent implements OnInit {
     })
   }
 
+  findByTituloPostagem(){
+
+    if(this.tituloPost== ''){
+    this.getAllPostagens()
+  } else{
+
+    this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp : Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
+  }
+
+  findByNomeTema(){
+
+    if(this.nomeTema == ''){
+     this.getAllTemas 
+    } else{
+      this.temaService.getByNomeTema(this.nomeTema).subscribe((resp: Tema[])=>{
+        this.listaTemas=resp
+      })
+    }
+  }
+  
 }
